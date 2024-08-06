59925e772d7d1683b997fd6c27903e961dc00422
Tanya Ivato <tetiana.ivato@gmail.com>
2024-08-06 19:40:44 +0300

    Minor fix

--- src/app/hoc/withVerticalLayout.tsx
+++ src/app/hoc/withVerticalLayout.tsx
@@ -1,6 +1,6 @@
-import { ComponentType } from 'react';
+import { ComponentType, JSX } from 'react';
 
-const withVerticalLayout = <T,>(WrappedComponent: ComponentType<T>) => {
+const withVerticalLayout = <T extends {}>(WrappedComponent: ComponentType<T>) => {
   return function WithVerticalLayout(props: T) {
     return (
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-4">
