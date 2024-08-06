436f02bea9b89f3a8b25096a36ffdda8a41cfb1d
Tanya Ivato <tetiana.ivato@gmail.com>
2024-08-06 18:36:20 +0300

    Disabled temporarely middleware check

--- src/app/signIn/signinSchema.ts
+++ src/app/signIn/signinSchema.ts
@@ -1,7 +1,6 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const signinSchema = z.object({
-    username: z.string().min(1, 'Username is required'),
-    password: z.string().min(6, 'Password must be at least 6 characters long'),
-  });
-  
+  username: z.string().min(1, 'Username is required'),
+  password: z.string().min(6, 'Password must be at least 6 characters long'),
+});
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,13 @@ import { getServerSession } from 'next-auth';
 import { options } from './app/api/auth/[...nextauth]/options';
 
 export async function middleware(req: NextRequest) {
-  const session = await getServerSession(options);
-
-  // If there is no session, redirect to the signIn page
-  if (!session?.user) {
-    return NextResponse.redirect(new URL('/signin', req.url));
-  }
-
-  return NextResponse.next();
+  //   const session = await getServerSession(options);
+  //   // If there is no session, redirect to the signIn page
+  //   if (!session?.user) {
+  //     return NextResponse.redirect(new URL('/signin', req.url));
+  //   }
+  //   return NextResponse.next();
+  // }
+  // export const config = {
+  //   matcher: ['/movies/[:id]', '/shows/[:id]', '/episodes/[:id]'],
 }
-
-export const config = {
-  matcher: ['/movies/[:id]', '/shows/[:id]', '/episodes/[:id]'],
-};
