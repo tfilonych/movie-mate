26fd93b3f0d8d3da2a45993379e5b8292d249e27
Tanya Ivato <tetiana.ivato@gmail.com>
2024-09-03 12:15:03 +0300

    Changed db var name

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -11,5 +11,3 @@ declare const globalThis: {
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
